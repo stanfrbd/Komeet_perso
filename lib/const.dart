5158662dfe8943add634d44627e832c85d77cf6a
stanfrbd <stanislas.medrano@u-psud.fr>
2019-05-31 09:59:41 +0200

    updated colors, firebase settings

--- lib/const.dart
+++ lib/const.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
+import 'package:flutter/material.dart';
 
 final themeColor = Color(0xff7289da);//Color(0xfff5a623);
 final primaryColor = Color(0xff203152);
 final greyColor = Color(0xffaeaeae);
 final greyColor2 = Color(0xffE8E8E8);
+
